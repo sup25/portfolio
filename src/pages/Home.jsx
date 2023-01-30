3fefaa7554cc513ff45f3d39f9d03bf992161fb7
sup25 <suparnaa51@gmail.com>
2023-01-30 22:39:02 +0545

    fifth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,38 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
-
-const Home = ({ theme }) => {
+import sup from '../sup.png'
+const Home = () => {
     return (
-        <Box>hello boy</Box>
+        <Box sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+
+
+        }}>
+            <Box sx={{}}>
+                <Typography sx={{ fontWeight: 'bold', fontSize: "14px" }}>
+                    ILLUSTRATOR, GRAPHIC DESIGNER
+                </Typography>
+                <Typography sx={{ fontWeight: 'bold', fontSize: "72px" }}>
+                    I design digital <br /> crafts for clients.
+
+                </Typography>
+            </Box>
+
+
+            <Box sx={{ display: { xs: 'none', md: 'flex', }, mr: 1 }}>
+                <img src={sup} alt="heroimage" style={{ width: "650px" }} />
+            </Box>
+
+
+
+
+
+
+
+
+        </Box>
     )
 }
 
