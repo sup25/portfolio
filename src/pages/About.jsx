a8552131d03c490fee363391b58395953f656fa1
sup25 <suparnaa51@gmail.com>
2023-01-31 22:30:25 +0545

    minor

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 const About = () => {
     return (
 
-        <Box id="about">About</Box>
+        <Box id="about">
+            {/* <Typography>
+As an Illustrator:
+
+I can create various types of illustrations such as logos, icons, illustrations, and more
+I have the ability to understand color, composition, and design principles and apply them in my work.
+I can use vector graphics editing program to create illustrations.
+
+As a Graphic Designer:
+
+I can design visual concepts for a variety of mediums including print materials, websites, advertising, packaging, etc.
+I have a strong understanding of typography, color theory, and design principles which I use to create visually appealing and effective designs that meet the client's needs.
+I can use computer software to create designs, or I can communicate ideas by hand.
+</Typography> */}
+
+        </Box>
     )
 }
 
